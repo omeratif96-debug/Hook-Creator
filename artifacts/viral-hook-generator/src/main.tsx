76721f5e750427a5d036e471d631a98c74720ff4
root <root@srv1489111.hstgr.cloud>
2026-03-23 17:58:20 +0000

    Cleanup: remove OpenClaw test logs

--- artifacts/viral-hook-generator/src/main.tsx
+++ artifacts/viral-hook-generator/src/main.tsx
@@ -1,6 +1,3 @@
-console.log("OpenClaw successfully connected to HookLab");
-console.log("Second test from OpenClaw");
-
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
